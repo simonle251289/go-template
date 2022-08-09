97b30fe91b3c6514ccb109bddab5a1f45375417a
Simon Lê <107181711+simonle251289@users.noreply.github.com>
2022-08-09 21:38:46 +0700

    - Add listen serve with custom port

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"log"
+	"strconv"
 	"template/components/appcontext"
 	"template/configs"
 	"template/middleware"
@@ -16,7 +17,7 @@ func main() {
 	appCtx := appcontext.NewAppContext(&config)
 	//Register route
 	auth.RegisterAuthRoute(appCtx, r)
-	if err := r.Run(); err != nil {
+	if err := r.Run("127.0.0.1:" + strconv.Itoa(config.Port)); err != nil {
 		log.Fatalln(err)
 	}
 }
